ddd779eaf06eb4fcdcc30d2df3d5c28e10f5e660
Jason Thomas <jthomas3853@icloud.com>
2025-04-20 21:29:59 -0400

    new site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Experienced Paid Media Specialists";
+export const SITE_DESCRIPTION = "Whether Google Ads, Facebook, Instagram, X (Twitter) or whichever, the goal is to bring in money. Our goals are aligned with yours. Learn More!";
+export const SITE_IMAGE =  "/site_thumbnail.bmp.svg";
+export const SITE_COMPANY = "Carolina Digtial";
