7ac199af8e40bd72fc0f054f95df483820d1a2e6
Jason Thomas <jthomas3853@icloud.com>
2025-09-14 16:20:57 -0400

    Update robots.txt.ts

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,22 +1,13 @@
- import type { APIRoute } from 'astro';
+import type { APIRoute } from 'astro';
 
-    const getRobotsTxt = (sitemapURL: URL) => `
+const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
 Allow: /
+
 Sitemap: ${sitemapURL.href}
 `;
 
-    export const GET: APIRoute = ({ site }) => {
-      // Ensure 'site' is defined in your astro.config.mjs
-      if (!site) {
-        throw new Error("Astro 'site' configuration is required to generate robots.txt dynamically.");
-      }
-      
-      const sitemapURL = new URL('/sitemap-index.xml', site); // Adjust sitemap-index.xml if your sitemap name is different
-
-      return new Response(getRobotsTxt(sitemapURL), {
-        headers: {
-          'Content-Type': 'text/plain; charset=utf-8',
-        },
-      });
-    };
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL('sitemap-index.xml', site);
+  return new Response(getRobotsTxt(sitemapURL));
+};
